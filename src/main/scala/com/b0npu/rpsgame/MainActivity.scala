47806ebc3ada19f9ee2b83ec95fd7dc2cdf0e1fd
b0mpu <b0npu@outlook.com>
2016-08-13 14:38:36 +0900

    First commit

---
+++ src/main/scala/com/b0npu/rpsgame/MainActivity.scala
@@ -0,0 +1,97 @@
+package com.b0npu.rpsgame
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.{ ImageButton, ImageView, TextView }
+
+class MainActivity extends AppCompatActivity with TypedFindView {
+
+    /**
+     * フィールドの定義
+     *
+     * widgetのidを格納する変数とじゃんけんで使う変数を定義する
+     */
+    var rockButton: ImageButton = _
+    var scissorButton: ImageButton = _
+    var paperButton: ImageButton = _
+    var playerMove: ImageView = _
+    var enemyMove: ImageView = _
+    var resultText: TextView = _
+
+    val handSigns = Map[String, Int](
+        "Rock" → R.drawable.rock,
+        "Scissor" → R.drawable.scissor,
+        "Paper" → R.drawable.paper
+    )
+    val handsList = handSigns.keys.toList
+
+    /**
+     * アプリの画面を生成
+     *
+     * アプリを起動するとonCreateが呼ばれてActivityが初期化され
+     * setContentViewでレイアウトがビューに表示される
+     */
+    override def onCreate( savedInstanceState: Bundle ): Unit = {
+        super.onCreate( savedInstanceState )
+        setContentView( R.layout.activity_main )
+
+        /**
+         * widgetのidを取得
+         *
+         * sbt-androidプラグインのTyped Resources(TR)を使って
+         * レイアウトに設置したwidgetのidを変数に格納する
+         */
+        rockButton = findView( TR.rockButton )
+        scissorButton = findView( TR.scissorButton )
+        paperButton = findView( TR.paperButton )
+
+        playerMove = findView( TR.playerMove )
+        enemyMove = findView( TR.enemyMove )
+        resultText = findView( TR.resultText )
+
+        /**
+         * ButtonをClickしてじゃんけん
+         *
+         * ClickしたButtonのImageをRPSGameメソッドに渡してじゃんけんする
+         */
+        rockButton.setOnClickListener( new OnClickListener() {
+            override def onClick( v: View ): Unit = {
+                RPSGame( handsList.indexOf( "Rock" ) )
+            }
+        } )
+        scissorButton.setOnClickListener( new OnClickListener() {
+            override def onClick( v: View ): Unit = {
+                RPSGame( handsList.indexOf( "Scissor" ) )
+            }
+        } )
+        paperButton.setOnClickListener( new OnClickListener() {
+            override def onClick( v: View ): Unit = {
+                RPSGame( handsList.indexOf( "Paper" ) )
+            }
+        } )
+    }
+
+    /**
+     * じゃんけんのメソッド
+     *
+     * 引数でplayerのhandSignを受け取ってRandomでenemyのhandSignを決める
+     * handSignはRock: 0, Scissor: 1, Paper: 2の数字を割り当てているので
+     * ((playerSign - enemySign) + 3 ) % 3の結果で勝敗が決まる
+     * 0: Draw, 1: Lose, 2: Win
+     */
+    private def RPSGame( playerSign: Int ): Unit = {
+        val gameResult = Map[Int, String](
+            0 → "Draw",
+            1 → "Oh You Lose...",
+            2 → "You Win!!"
+        )
+        val enemySign = scala.util.Random.nextInt( 3 )
+        val result = ( playerSign - enemySign + 3 ) % 3
+
+        playerMove.setImageResource( handSigns( handsList( playerSign ) ) )
+        enemyMove.setImageResource( handSigns( handsList( enemySign ) ) )
+        resultText.setText( gameResult( result ) )
+    }
+}
